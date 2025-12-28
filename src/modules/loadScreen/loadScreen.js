539a33e2905f0773323b5857b65dc6ea41b8f069
IsmaelPeVe <iperezvg@fpvirtualaragon.es>
2025-12-28 18:34:10 +0100

    - Depurado error en la carga del logo, el cául mandaba un error en la promesa.
    Manejado a partir de un resolveAssetSource (linea 54 de loadScreen)

--- src/modules/loadScreen/loadScreen.js
+++ src/modules/loadScreen/loadScreen.js
@@ -1,7 +1,10 @@
+import { Scene } from "@babylonjs/core";
 import { Rectangle, Image, Control, TextBlock } from "@babylonjs/gui";
 import { getGlobalUI, clearGlobalUI } from "../../utils/uiManager.js";
 import { bigNormalText, normalText } from "../../components/textFormat.js";
-
+import logoAsset from "../../assets/Pueven_logo.png";
+import { Image as RNImage } from "react-native";
+import { universalCamera } from "../../components/Camera.js";
 /**
  * Variable local para guardar el contenedor de la pantalla de carga.
  * @type {Rectangle|null}
@@ -10,9 +13,28 @@ import { bigNormalText, normalText } from "../../components/textFormat.js";
 let loadingContainer = null;
 
 /**
+ * Crea la escena inicial de Babylon.js y activa la interfaz de carga.
+ * Esta función es el punto de entrada principal que invoca el motor de Babylon.js
+ * al arrancar la aplicación en el dispositivo móvil.
+ *
+ * @param {import("@babylonjs/core").Engine} engine - El motor de renderizado de Babylon.
+ * @returns {Scene} La escena de carga inicial.
+ */
+export function createScene(engine) {
+    const scene = new Scene(engine);
+
+    const camera = universalCamera(scene);
+    camera.position.z = -5;
+    // Ejecuta la pantalla de carga
+    showLoadScreen(scene);
+
+    return scene;
+}
+
+/**
  * Crea y muestra la pantalla de carga con el logo y el mensaje de bienvenida.
- * * Esta función prepara la primera impresión del usuario, configurando el fondo,
- * el logo del centro comercial y los textos animados.
+ * Esta función prepara la primera impresión del usuario, configurando el fondo,
+ * el logo del centro comercial y los textos.
  *
  * @param {import("@babylonjs/core").Scene} scene - La escena donde se creará la interfaz.
  * @returns {void}
@@ -26,13 +48,15 @@ export function showLoadScreen(scene) {
     loadingContainer.height = "100%";
     loadingContainer.thickness = 0;
     loadingContainer.background = "#0A0A0A";
+    ui.addControl(loadingContainer);
 
     // Imagen del Logo.
-    const logo = new Image("logo", "assets/Pueven_logo.png");
+    const resolvedLogo = RNImage.resolveAssetSource(logoAsset);
+    const logo = new Image("logo", resolvedLogo.uri);
     logo.width = "250px";
     logo.height = "250px";
     logo.verticalAlignment = Control.VERTICAL_ALIGNMENT_CENTER;
-    logo.top = "-50px";
+    logo.top = "50px";
     loadingContainer.addControl(logo);
 
     // Texto de Bienvenida 
@@ -40,24 +64,27 @@ export function showLoadScreen(scene) {
     welcomeText.color = "#00E5FF";
     welcomeText.verticalAlignment = Control.VERTICAL_ALIGNMENT_CENTER;
     welcomeText.top = "100px";
+
+    // Aplicación de escala estándar del texto.
     bigNormalText(welcomeText);
     loadingContainer.addControl(welcomeText);
 
     // Texto de estado "Cargando..."
-    const statusText = new TextBlock("statusText", "CARGANDO SISTEMAS...");
+    const statusText = new TextBlock("statusText", "CARGANDO...");
     statusText.color = "#BC00FF";
     statusText.verticalAlignment = Control.VERTICAL_ALIGNMENT_BOTTOM;
     statusText.top = "-60px";
+
+    // Aplicación de escala estándar del texto.
     normalText(statusText);
     loadingContainer.addControl(statusText);
 
-    ui.addControl(loadingContainer);
     console.log("Pantalla de carga mostrada...");
 }
 
 /**
  * Simula el progreso de carga o muestra un mensaje específico de sistema.
- * Útil para dar feedback al usuario mientras se inician los módulos AR.
+ * Útil para dar feedback al usuario.
  *
  * @param {string} message - El mensaje que se quiere mostrar.
  */
@@ -72,8 +99,8 @@ export function updateLoadStatus(message) {
 
 /**
  * Elimina la pantalla de carga para dar paso a la aplicación principal.
- * * Es vital llamar a esta función cuando el mapa o la escena AR estén listos
- * para liberar memoria y permitir la interacción del usuario.
+ * Es vital llamar a esta función cuando el mapa o la escena AR estén listos
+ * para liberar memoria.
  */
 export function disposeLoadScreen() {
     if (loadingContainer) {
